0e2cc7154b5612a40f37299e9a4d943cc86e7160
JussiIH <48416422+JussiIH@users.noreply.github.com>
2019-04-22 19:14:06 +0300

    Add files via upload

--- Omena.java
+++ Omena.java
@@ -1,1 +1,38 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
+public class Omena{
+	private int xKoord, yKoord, leveys, korkeus;
+	
+	public Omena(int xKoord, int yKoord, int tileSize){
+		this.xKoord = xKoord;
+		this.yKoord = yKoord;
+		leveys = tileSize;
+		korkeus = tileSize;
+	}
+	
+	public void tick(){
+		
+	}
+	
+	public void draw(Graphics g){
+		g.setColor(Color.GREEN);
+		g.fillRect(xKoord * leveys, yKoord * korkeus, leveys, korkeus);
+	} 
+	
+	public int annaxKoord(){
+		return xKoord;
+	}
+	
+	public void asetaxKoord(int xKoord){
+		this.xKoord = xKoord;
+	} 
+	
+	public int annayKoord(){
+		return yKoord;
+	}
+	
+	public void asetayKoord(int yKoord){
+		this.yKoord = yKoord;
+	}
+} 
